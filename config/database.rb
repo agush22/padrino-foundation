0f179369837f4b2c7328fa83729fc933a2b8554b
Agustín Leñero <agush@roca9.com>
2014-09-04 00:08:28 -0500

    Configure db for production on heroku and change db for dev and test

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,7 @@
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("postgres://localhost/roca_development", :loggers => [logger])
-  when :production  then Sequel.connect("postgres://localhost/roca_production",  :loggers => [logger])
-  when :test        then Sequel.connect("postgres://localhost/roca_test",        :loggers => [logger])
+  when :development then Sequel.connect("postgres://agush:@localhost/roca9_development", :loggers => [logger])
+  when :test        then Sequel.connect("postgres://agush:@localhost/roca9_test", :loggers => [logger])
+  when :production  then Sequel.connect(ENV['DATABASE_URL'], :loggers => [logger])
 end
